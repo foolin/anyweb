4ae048ecc8f3d797fde4b5e068deb84a97fcb529
liufuling@msn.com <liufuling@msn.com@782f7090-3c72-11de-801c-c7cb65f9a556>
2009-09-16 00:57:10 +0000

    更新一下风格。

---
+++ trunk/Web/JS/fontzoom.js
@@ -0,0 +1,19 @@
+var curFontSize = 14;				//当前字体大小
+var fontAreaId = 'art-content';		//文字区域的ID
+/* 大 */
+function fontZoomMax(){
+	if(curFontSize < 64){
+		document.getElementById(fontAreaId).style.fontSize = (++curFontSize)+'px';
+	}
+}
+/* 中 */
+function fontZoomMid(){
+		curFontSize = 14;
+		document.getElementById(fontAreaId).style.fontSize = curFontSize + 'px';
+}
+/* 小 */
+function fontZoomMin(){
+	if(curFontSize > 8){
+		document.getElementById(fontAreaId).style.fontSize = (--curFontSize)+'px';
+	}
+}

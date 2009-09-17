93fda578fc454f8dec5e07291578ba21940e728e
liufuling@msn.com <liufuling@msn.com@782f7090-3c72-11de-801c-c7cb65f9a556>
2009-09-17 04:08:40 +0000

    增加更新后EventLog，忘记了更新该模块。

--- trunk/AnyWeb_DL/Agent/EventLogAgent.cs
+++ trunk/AnyWeb_DL/Agent/EventLogAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Studio.Data;
 using System.Data;
+using System.Collections;
 
 namespace AnyWeb.AnyWeb_DL
 {
@@ -25,5 +26,61 @@ namespace AnyWeb.AnyWeb_DL
                     this.NewParam("@EvenUserAcc", log.EvenUserAcc));
             }
         }
+
+        /// <summary>
+        /// 获取事件日志列表
+        /// </summary>
+        /// <param name="EvenUserAcc"></param>
+        /// <param name="PageSize"></param>
+        /// <param name="PageNo"></param>
+        /// <param name="Record"></param>
+        /// <returns></returns>
+        public ArrayList GetLogList(string EvenUserAcc, int PageSize, int PageNo, out int Record)
+        {
+            DataSet ds;
+            using (IDbExecutor db = this.NewExecutor())
+            {
+                IDbDataParameter record = this.NewParam("@RecordCount", 0, DbType.Int32, 4, true);
+                ds = db.GetDataSet(CommandType.StoredProcedure, "GetLogList",
+                    this.NewParam("@EvenUserAcc", EvenUserAcc),
+                    this.NewParam("@PageSize", PageSize),
+                    this.NewParam("@PageNo", PageNo),
+                    record);
+                Record = (int)record.Value;
+            }
+
+            ArrayList list = new ArrayList();
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                EventLog log = new EventLog(row);
+                list.Add(log);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 批量删除事件记录
+        /// </summary>
+        /// <param name="ids"></param>
+        public void DeleteLog(string ids)
+        {
+            using (IDbExecutor db = this.NewExecutor())
+            {
+                db.ExecuteNonQuery(CommandType.StoredProcedure, "DeleteLogs",
+                    this.NewParam("@ids", ids));
+            }
+        }
+
+        /// <summary>
+        /// 清空记录
+        /// </summary>
+        public void ClearLog()
+        {
+            using (IDbExecutor db = this.NewExecutor())
+            {
+                db.ExecuteNonQuery(CommandType.StoredProcedure, "ClearLogs");
+            }
+        }
+
     }
 }

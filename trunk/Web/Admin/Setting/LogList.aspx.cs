507e33c3eb735e77204c175cfbd1944cea04cff6
liufuling@msn.com <liufuling@msn.com@782f7090-3c72-11de-801c-c7cb65f9a556>
2009-09-17 02:06:10 +0000

    增加操作日志管理功能。

--- trunk/Web/Admin/Setting/LogList.aspx.cs
+++ trunk/Web/Admin/Setting/LogList.aspx.cs
@@ -8,11 +8,30 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using AnyWeb.AnyWeb_DL;
 
 public partial class Admin_Setting_LogList : AdminBase
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected override void OnPreRender(EventArgs e)
+    {
+        drpUser.DataSource = new UserAgent().GetAllUserList();
+        drpUser.DataBind();
+        drpUser.Items.Insert(0, new ListItem("所有用户", ""));
+
+        if (QS("u") != "")
+            drpUser.SelectedValue = QS("u");
+
+        int RecordCount = 0;
+        repEventLog.DataSource = new EventLogAgent().GetLogList(drpUser.SelectedValue, PN1.PageSize, PN1.PageIndex, out RecordCount);
+        repEventLog.DataBind();
+        PN1.SetPage(RecordCount);
+    }
+    protected void SelectUser(object sender, EventArgs e)
+    {
+        Response.Redirect("/Admin/Setting/LogList.aspx?u=" + drpUser.SelectedValue);
     }
 }

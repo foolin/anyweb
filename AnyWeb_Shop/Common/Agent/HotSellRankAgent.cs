84f0b7540017fd2847cb6626132b7065c8f7b664
liufuling@msn.com <liufuling@msn.com@782f7090-3c72-11de-801c-c7cb65f9a556>
2010-03-20 08:51:43 +0000

    更新相关函数

---
+++ AnyWeb_Shop/Common/Agent/HotSellRankAgent.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+using Studio.Data;
+using System.Data;
+using Common.Common;
+using System.Web;
+
+namespace Common.Agent
+{
+    public class HotSellRankAgent : AgentBase
+    {
+        /// <summary>
+        /// 获取商品列表
+        /// </summary>
+        /// <param name="recordCount"></param>
+        /// <returns></returns>
+        public ArrayList GetHotSellRankList(out int recordCount)
+        {
+            DataSet ds = new DataSet();
+
+            IDbDataParameter record = this.NewParam("@RecordCount", 0, DbType.Int32, 8, true);
+
+            using (IDbExecutor db = this.NewExecutor())
+            {
+                ds = db.GetDataSet(CommandType.StoredProcedure, "Shop_GetHotSellRankList", record);
+            }
+            recordCount = (int)record.Value;
+
+            ArrayList list = new ArrayList();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                HotSellRank hsr = new HotSellRank();
+                hsr.HotSellID = (int)dr["HotSellID"];
+                hsr.GoodsID = (int)dr["GoodsID"];
+                hsr.Sort = (short)dr["Sort"];
+                hsr.OfGoods = new Goods();
+                hsr.OfGoods.GoodsName = (string)dr["GoodsName"];
+                hsr.OfGoods.Image = (string)dr["Image"];
+                hsr.OfGoods.Price = (double)dr["Price"];
+                hsr.OfGoods.MarketPrice = (double)dr["MarketPrice"];
+                hsr.OfGoods.Status = (int)dr["Status"];
+                list.Add(hsr);
+            }
+
+            return list;
+        }
+
+
+        /// <summary>
+        /// 获取畅销商品数
+        /// </summary>
+        /// <returns></returns>
+        public int GetGoodsCount()
+        {
+            int count = 0;
+            this.GetHotSellRankList(out count);
+            return count;
+        }
+
+        /// <summary>
+        /// 添加商品
+        /// </summary>
+        /// <param name="addIds"></param>
+        /// <returns></returns>
+        public int AddGoods(string addIds)
+        {
+            string[] arrIds = addIds.Split(",".ToCharArray());  //按逗号将数据分割成一个数组
+            int result = 0;
+            for (int i = 0; i < arrIds.Length; i++)
+            {
+                using (IDbExecutor db = this.NewExecutor())
+                {
+                    db.ExecuteNonQuery(CommandType.StoredProcedure, "Shop_HotSellRankAdd",
+                                        this.NewParam("@GoodsId", (int)addIds[i]),
+                                        this.NewParam("@Sort", 0));
+                    result++;
+                }
+            }
+            return result;
+
+        }
+
+ 
+
+
+        /// <summary>
+        /// 删除畅销商品
+        /// </summary>
+        /// <param name="delIds"></param>
+        /// <returns></returns>
+        public int DeleteGoods(string delIds)
+        {
+            int result;
+            using (IDbExecutor db = this.NewExecutor())
+            {
+                result = db.ExecuteNonQuery(CommandType.StoredProcedure, "Shop_HotSellRankDelete",
+                                            this.NewParam("@Ids", delIds));
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ AnyWeb_Shop/Common/Common/HotSellRank.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Studio.Array;
+using Studio.Data;
+using System.Data;
+using Common.Common;
+using System.Web;
+namespace Common.Common
+{
+    class HotSellRank: IdObject
+    {
+        public HotSellRank()
+        { 
+        }
+        public HotSellRank(DataRow dr)
+        {
+            this.HotSellID = (int)dr["HotSellID"];
+            this.GoodsID = (int)dr["GoodsID"];
+            this.Sort = (short)dr["Sort"];
+        }
+
+        private int _hotSellID;
+        /// <summary>
+        /// 热门销售排行ID
+        /// </summary>
+        public int HotSellID
+        {
+            get { return _hotSellID; }
+            set { _hotSellID = value; }
+        }
+
+
+        private int _goodsId;
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int GoodsID
+        {
+            get { return _goodsId; }
+            set { _goodsId = value; }
+        }
+
+        private short _sort;
+        /// <summary>
+        /// 商品排序
+        /// </summary>
+        public short Sort
+        {
+            get { return _sort; }
+            set { _sort = value; }
+        }
+
+        private Goods _ofGoods;
+        /// <summary>
+        /// 商品
+        /// </summary>
+        public Goods OfGoods
+        {
+            get { return _ofGoods; }
+            set { _ofGoods = value; }
+        }
+        
+    }
+}
